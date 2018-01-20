2d056c001be2f88fc775ad4ebee1d3b7b50fbd62
Christian Nikolov <christian@newton.codes>
2018-01-20 04:57:33 +0200

    Remove the --no-cache option

--- dev/build.sh
+++ dev/build.sh
@@ -2,5 +2,5 @@
 
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-cd ${dir}/.. && docker build --no-cache -t newtoncodes/iredmail .
-cd ${dir}/.. && docker build --no-cache -t newtoncodes/iredmail:0.9.7 .
+cd ${dir}/.. && docker build -t newtoncodes/iredmail .
+cd ${dir}/.. && docker build -t newtoncodes/iredmail:0.9.7 .
